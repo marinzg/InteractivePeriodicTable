26b8c30664f374f6e3e9cf656be38c7f7edbb53a
maranmaran <mu47766@fer.hr>
2016-04-01 13:29:40 +0200

    Jos radim na prikazu HTML-a, u funkciji s textBoxom dodao sam komentar u vezi Highlightanja pa ako ce Petar raditi moze nastaviti, dodao funkciju za pretrazivanje kontroli na formi

--- InteractivePeriodicTable/InteractivePeriodicTable/MainWindow.xaml.cs
+++ InteractivePeriodicTable/InteractivePeriodicTable/MainWindow.xaml.cs
@@ -50,14 +50,57 @@ namespace InteractivePeriodicTable
                 listBox.Items.Add(match.Value.ToString());
                 match = match.NextMatch();
                 listBox.Visibility = Visibility.Visible;
+
+                //Petar probaj skombat nes sa sljedecim sto se tice highlightanja
+
+                /*  foreach(Button searchedElement in FindVisualChildren<Button>(this))
+                    {
+                         if(match.ToString()==searchedElement.Name.ToString())
+                            {
+                                searchedElement.BorderBrush = Brushes.Red;
+                                searchedElement.BorderThickness = new Thickness(1000, 1000, 1000, 1000);
+                                searchedElement.Background = Brushes.Red;
+                                }
+
+                      }
+                 */
+
+
             }
 
-            if (listBox.Items.IsEmpty || listBox.Items.Count==119)
+            if (listBox.Items.IsEmpty || listBox.Items.Count == 119)
             {
                 listBox.Visibility = Visibility.Collapsed;
             }
+
+
+
+
         }
 
+        //S ovom funkcijom mozemo pretrazivati kontrole na formi 
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+
+
     }
-   
+
 }
