4d2be1180a061e49ff1b5856feb5c1ed674457f2
Eraz1311 <perozder0>
2016-04-13 15:56:57 +0200

    y

--- InteractivePeriodicTable/InteractivePeriodicTable/Quiz.xaml.cs
+++ InteractivePeriodicTable/InteractivePeriodicTable/Quiz.xaml.cs
@@ -101,6 +101,8 @@ namespace InteractivePeriodicTable
             Label question_lbl = new Label();
             question_lbl.Content = picked_question.Question;
             question_lbl.Foreground = Brushes.Blue;
+            question_lbl.FontSize = 14;
+            question_lbl.FontWeight = FontWeights.SemiBold;
 
             Button A1 = new Button();
             A1.Content = picked_question.A1;
@@ -160,6 +162,8 @@ namespace InteractivePeriodicTable
             Label question_lbl = new Label();
             question_lbl.Content = picked_question.Question;
             question_lbl.Foreground = Brushes.Blue;
+            question_lbl.FontSize = 14;
+            question_lbl.FontWeight = FontWeights.SemiBold;
 
             Button A1 = new Button();
             A1.Content = picked_question.A1;
@@ -191,6 +195,9 @@ namespace InteractivePeriodicTable
 
             Label lbl = new Label();
             lbl.Content = "Write what you see in this image";
+            lbl.FontSize = 14;
+            lbl.FontWeight = FontWeights.SemiBold;
+            lbl.Foreground = Brushes.Blue;
 
             Image image = new Image();
             image.Width = 300;
@@ -205,6 +212,8 @@ namespace InteractivePeriodicTable
             TextBox txbx = new TextBox();
             txbx.Name = "QuizPictures_txbx";
             txbx.TextAlignment = TextAlignment.Center;
+            txbx.Width = 120;
+            txbx.Margin = new Thickness(0, 5, 0, 5);
 
             this.sp.RegisterName(txbx.Name, txbx);
 
